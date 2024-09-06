d6d0f2eb8a320bceecc467fd82822321116102bd
aliakbaresmaeiliiii <aliakbaresmaeilisoft@gmail.com>
2024-09-06 09:12:26 +0800

    fix link button

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,11 +3,12 @@ import { RegisterComponent } from '../../auth/register/register.component';
 import { FooterComponent } from '../footer/footer.component';
 import { MenuComponent } from '../menu/menu.component';
 import { SliderComponent } from '../slider/slider.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RegisterComponent, FooterComponent, MenuComponent, SliderComponent],
+  imports: [RegisterComponent, FooterComponent, MenuComponent, SliderComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/modules/slider/slider.component.ts
+++ src/app/modules/slider/slider.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
-export class SliderComponent {
-
-}
+export class SliderComponent {}
